7bfcc80c62fec98f3013f84d1c3018d5ad1d0c4c
[Lukass48] <pielvitorilucas@gmail.com>
2024-03-04 19:20:23 -0300

    Hw 1 proyecto integrador M2

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,19 @@
 console.log(tempData);
+
+const movieContainer = document.getElementById('movieContainer');
+
+tempData.forEach(movie => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+        <img src="${movie.poster}" alt="${movie.title}" class="cardImg">
+        <div class="cardContainer">
+            <h5 class="cardTitle">${movie.title} (${movie.year})</h5>
+            <p class="cardText"><strong>Director:</strong> ${movie.director}</p>
+            <p class="card-text"><strong>Duration:</strong> ${movie.duration}</p>
+            <p class="card-text"><strong>Genre:</strong> ${movie.genre}</p>
+            <p class="card-text"><strong>rate:</strong> ${movie.rate}</p>
+        </div>
+    `;
+    movieContainer.appendChild(card);
+})
