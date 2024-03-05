df753f038a39c456e0c43459b3f9b6ad0af1d233
[Lukass48] <pielvitorilucas@gmail.com>
2024-03-05 20:15:43 -0300

    api

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,21 +1,23 @@
-console.log(tempData);
+function getMovies() {
+    $.get('https://students-api.2.us-1.fl0.io/movies', function(data) {
+        const movieContainer = $('#movieContainer');
+        const cards = data.map(movie => {
+            const card = $('<div>').addClass('card').html(`
+                <img src="${movie.poster}" alt="${movie.title}" class="cardImg">
+                <div class="cardContainer">
+                    <h5 class="cardTitle">${movie.title} (${movie.year})</h5>
+                    <p class="cardText"><strong>Director:</strong> ${movie.director}</p>
+                    <p class="card-text"><strong>Duration:</strong> ${movie.duration}</p>
+                    <p class="card-text"><strong>Genre:</strong> ${movie.genre}</p>
+                    <p class="card-text"><strong>Rate:</strong> ${movie.rate}</p>
+                </div>
+            `);
+            return card;
+        });
+        movieContainer.empty().append(cards);
+    }).fail(function(error) {
+        console.error('Error fetching movies:', error);
+    });
+}
 
-const movieContainer = document.getElementById('movieContainer');
-
-const cards = tempData.map(movie => {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.innerHTML = `
-        <img src="${movie.poster}" alt="${movie.title}" class="cardImg">
-        <div class="cardContainer">
-            <h5 class="cardTitle">${movie.title} (${movie.year})</h5>
-            <p class="cardText"><strong>Director:</strong> ${movie.director}</p>
-            <p class="card-text"><strong>Duration:</strong> ${movie.duration}</p>
-            <p class="card-text"><strong>Genre:</strong> ${movie.genre}</p>
-            <p class="card-text"><strong>rate:</strong> ${movie.rate}</p>
-        </div>
-    `;
-    return card;
-    
-})
-movieContainer.append(...cards);
+getMovies();
