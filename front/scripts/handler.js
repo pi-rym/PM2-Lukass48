9f7d5741cc1fae1a618bac934796819997f9bfb6
[Lukass48] <pielvitorilucas@gmail.com>
2024-03-11 18:31:21 -0300

    asincronismo

--- front/scripts/handler.js
+++ front/scripts/handler.js
@@ -1,6 +1,20 @@
-const renderMovies = require('./getmovies')
+// const renderMovies = require('./getmovies')
+const axios = require("axios")
 
-const getMovie = ()=> {
-    $.get('https://students-api.2.us-1.fl0.io/movies', (data) => data.forEach(renderMovies))
+// const getMovie = ()=> {
+//     $.get('https://students-api.2.us-1.fl0.io/movies', (data) => data.forEach(renderMovies))
+// }
+
+const fetchData = async()=>{
+    try {
+        const data = await axios.get('https://students-api.2.us-1.fl0.io/movies');
+        console.log(data.data)
+    } catch (error) {
+        throw new Error("No se encontro la API");
+        console.log(error)
+    }
+    
 }
-module.exports = getMovie
+fetchData()
+
+//  module.exports = getMovie
